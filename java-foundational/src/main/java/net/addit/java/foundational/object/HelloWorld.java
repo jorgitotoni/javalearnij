20f06a513c3fea9606cf35961881ea2bc1c19362
jorgitotoni <241963698@qq.com>
2022-06-23 23:00:50 +0800

    my first java learning project

---
+++ java-foundational/src/main/java/net/addit/java/foundational/object/HelloWorld.java
@@ -0,0 +1,14 @@
+package net.addit.java.foundational.object;
+
+/**
+ * my first java learning project
+ *
+ * @author tony 241963698@qq.com
+ * @version 2022/6/23 下午10:55
+ * @since JDK8
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
